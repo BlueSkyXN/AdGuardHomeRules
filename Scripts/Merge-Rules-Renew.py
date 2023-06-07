a517ca4eeb52a5c1b79ddb30f43cf6ec24f8dea5
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-07 16:28:24 +0800

    3.0.3

--- Scripts/Merge-Rules-Renew.py
+++ Scripts/Merge-Rules-Renew.py
@@ -1,17 +1,16 @@
-import os
+filenames = [
+    "Renew/cjx-annoyance.txt",
+    "Renew/anti-ad-adguard.txt",
+    "Renew/anti-ad-easylist.txt",
+    "Renew/easyprivacy.txt",
+    "Renew/easylist.txt",
+    "Renew/easylistchina.txt",
+    "Renew/halflife.txt",
+    "Renew/scamblocklist-host.txt",
+    "Renew/StevenBlack.txt"
+]
 
-# 定义文件名列表
-filenames = ["cjx-annoyance.txt", "anti-ad-adguard.txt", "anti-ad-easylist.txt", "easyprivacy.txt", "easylist.txt", "easylistchina.txt", "halflife.txt", "scamblocklist-host.txt", "StevenBlack.txt"]
-
-# 打开你想要写入的文件
-with open("Part/rules-renew.txt", 'w') as outfile:
-    # 遍历文件名列表
+with open('Part/rules-renew.txt', 'w', encoding='utf-8') as outfile:
     for fname in filenames:
-        # 为每个文件名创建完整路径
-        path = os.path.join("Renew", fname)
-        # 打开每个文件
-        with open(path) as infile:
-            # 读取文件内容并写入新文件
+        with open(fname, 'r', encoding='utf-8') as infile:
             outfile.write(infile.read())
-        # 在每个文件之间添加一个换行符，以确保文件的内容不会混在一起
-        outfile.write("\n")
